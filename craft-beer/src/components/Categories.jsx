7136ce0994d0f282c565cb406a382c772c99d60f
valer-sky <89993618+valer-sky@users.noreply.github.com>
2022-06-29 17:05:21 +0300

    add components

---
+++ craft-beer/src/components/Categories.jsx
@@ -0,0 +1,17 @@
+
+const Categories = () => {
+    return(
+      <div class="categories">
+              <ul>
+                <li class="active">Все</li>
+                <li>Эли светлые</li>
+                <li>Эли темные</li>
+                <li>Портер</li>
+                <li>Стаут</li>
+                <li>Sour Ale</li>
+              </ul>
+            </div>
+    )
+  }
+
+  export default Categories;
