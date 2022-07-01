548af065afd13eed643f259ad02bdcb4bfab7fcd
valer-sky <89993618+valer-sky@users.noreply.github.com>
2022-07-01 15:38:51 +0300

    add mockapi

--- craft-beer/src/App.js
+++ craft-beer/src/App.js
@@ -1,12 +1,25 @@
-
+import React from 'react';
 import './scss/app.scss';
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import BeerBlock from './components/BeerBlock';
-import beers from './assets/beers.json';
+
+
+
 
 function App() {
+  const [items, setItems] = React.useState([]);
+    React.useEffect(() => {
+      fetch('https://62becc69be8ba3a10d5be2ad.mockapi.io/items')
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+      });
+    
+    }, []);
+
+ 
   return (
     <div className="wrapper">
         <Header />
@@ -19,7 +32,7 @@ function App() {
         <h2 className="content__title">Всё пиво</h2>
         <div className="content__items">
           {
-            beers.map(obj =>
+            items.map(obj =>
             <BeerBlock key={obj.id}
               title={obj.title} 
               price={obj.price} 
